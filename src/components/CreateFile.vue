5fe22be7dd922139f7010d48da3e292049266af6
luwenyan <1030037201@qq.com>
2022-08-14 03:36:59 +0800

    1

--- src/components/CreateFile.vue
+++ src/components/CreateFile.vue
@@ -1,58 +1,58 @@
 <template>
   <ul>
     <li class="creatfile">
-        <div class="creattitle">创建问卷</div>
-        <div class="creatcontent">
-            <el-progress type="circle" :percentage="0" />
-        </div>
+      <div class="creattitle">创建问卷</div>
+      <div class="creatcontent">
+        <span class="progress">
+          <el-progress type="circle" :percentage="0" :width="90" :height="90" />
+        </span>
+      </div>
     </li>
   </ul>
 </template>
 
 <script setup>
-
 </script>
 
 <style>
-    ul li {
-        list-style: none
-    }
-    .creattitle {
-        position: relative;
-        width: 10rem;
-        height: 2.7rem;
-        border-radius: 4px 4px 0px 0px;
-        background:  rgba(138, 204, 237, 1) ;
-        text-align: left;
-        text-indent: 2rem;
-        line-height: 2.7rem;
-    }
-    .creattitle::after{
-        content: '';
-        position: absolute;
-        right: 0;
-        top: 0;
-        /* width: 2.7rem;
+ul li {
+  list-style: none;
+}
+.creattitle {
+  position: relative;
+  width: 10rem;
+  height: 2.7rem;
+  border-radius: 4px 4px 0px 0px;
+  background: rgba(138, 204, 237, 1);
+  text-align: left;
+  text-indent: 2rem;
+  line-height: 2.7rem;
+}
+.creattitle::after {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 0;
+  /* width: 2.7rem;
         height: 2.7rem; */
-        border-top: 1.35rem solid white;
-        border-right: 1.35rem solid white;
-        border-bottom: 1.35rem solid transparent;
-        border-left: 1.35rem solid transparent;
+  border-top: 1.35rem solid white;
+  border-right: 1.35rem solid white;
+  border-bottom: 1.35rem solid transparent;
+  border-left: 1.35rem solid transparent;
 
-        z-index: 999;
+  z-index: 999;
+}
+.creatcontent {
+  position: relative;
+  width: 21rem;
+  height: 12.1rem;
+  border-radius: 0px 4px 4px 4px;
+  background: rgba(138, 204, 237, 1);
+}
 
-    }
-    .creatcontent {
-        position: relative;
-        width: 21rem;
-        height: 12.1rem;
-        border-radius: 0px 4px 4px 4px;
-        background: rgba(138, 204, 237, 1);
-    }
-
-    .el-progress {
-        position: absolute;
-        top: 1.5rem;
-        right: 1.7rem;
-    }
+.progress {
+  position: absolute;
+  top: 1rem;
+  right: 0.5rem;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Pinia from '../components/piniaTest.vue'
 import login from '../components/login.vue'
+import statistics from '../components/statistics.vue'
 const routes = [
     { path: '/', component: Pinia },
     { path: '/login', component: login },
+    { path: '/', component: statistics },
 ]
 
 export default createRouter({
