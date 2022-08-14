9fd48ef0efd41df1e8047371098e8d27f33e4b11
MerrickCai <merrickcai.dev@gmail.com>
2022-08-14 12:44:46 +0800

    update

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -11,10 +11,11 @@ function backhome() {
 
 <template>
     <nav>
-        <div id="logo">
+        <div class="logo">
+            <img src="/logo.png" />
             <router-link to="/">问卷易</router-link>
         </div>
-        <div>
+        <div class="links">
             <router-link to="/pinia">pinia测试</router-link>
             <router-link to="/login">登录/注册</router-link>
             <router-link :to="`/${Math.floor(Math.random() * 100)}`">404页</router-link>
@@ -29,7 +30,6 @@ nav {
     flex-wrap: nowrap;
     justify-content: center;
     align-items: center;
-    margin: 0 auto;
     height: 60px;
     width: var(--safe_area);
     position: fixed;
@@ -37,28 +37,52 @@ nav {
     top: 0;
     left: calc((100% - var(--safe_area))/2);
 
-    #logo {
+    >div.logo {
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         justify-content: center;
         align-items: center;
-        height: auto;
-        width: 30%;
+        height: 100%;
+        width: 20%;
+
+        img {
+            display: block;
+            height: 100%;
+            width: auto;
+            object-fit: contain;
+        }
 
         a {
-            color: rgb(0, 142, 250);
-            font-size: 1.5rem;
+            display: block;
+            height: 100%;
+            width: auto;
+            line-height: 60px;
+            color: rgb(0, 0, 0);
+            font-size: 1.7rem;
+            text-decoration: none;
         }
     }
 
-    a {
-        display: inline-block;
-        margin: 5px;
-        padding: 5px;
-        text-decoration: none;
-        color: rgb(0, 142, 250);
-        font-size: 1rem;
+    >div.links {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: flex-end;
+        align-items: center;
+        height: 100%;
+        width: 80%;
+
+        a {
+            display: block;
+            height: 100%;
+            margin-right: 10px;
+            width: auto;
+            line-height: 60px;
+            color: rgb(0, 142, 250);
+            font-size: 1rem;
+            text-decoration: none;
+        }
     }
 }
 </style>
