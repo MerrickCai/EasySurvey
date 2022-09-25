3af27f417a12da71e7734bced48bd5f56e999d15
luwenyan <1030037201@qq.com>
2022-09-25 11:58:59 +0800

    日期

--- src/components/user/record.vue
+++ src/components/user/record.vue
@@ -1,61 +1,108 @@
 <template>
     <div class="wapper">
-        <div class="title">问卷记录</div>
+        <div class="top">
+            <div class="title">问卷记录</div>
+            <select name="" id="">
+                <option value="">全部问卷</option>
+                <option value="">已填写</option>
+                <option value="">已发布</option>
+            </select>
+            <div class="time">
+                <!-- <el-date-picker v-model="value1" type="date" placeholder="Pick a day" size="default" /> -->
+                <el-date-picker v-model="value2" type="daterange" range-separator="To" start-placeholder="Start date"
+                    end-placeholder="End date" size="default" value-format="YYYY-MM-DD"
+                    :disabled-date="disabledDateFun" />
+            </div>
+            <div @click="show">1111111</div>
+
+        </div>
+
     </div>
 </template>
 <script setup>
 import axios from "axios";
+import { ref, onMounted } from "vue"
 import { useStore } from "../../Stores/pinia.js";
 const datas = useStore();
-function getted() {
-    axios({
-        url: `https://q.denglu1.cn/api/user/questionnaireList/${datas.user.userId}`,
-        method: "get",
-        withCredentials: true,
-        headers: { "Content-Type": "application/json", token: datas.getToken() },
 
-    })
-        .then((response) => {
-            console.log("用户填写过的所有问卷信息", response);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+
+const value1 = ref('')
+const value2 = ref('')
+let show = () => {
+    console.log(value2.value);
 }
-getted();
-function getinfor() {
-    axios({
-        url: `https://q.denglu1.cn/api/user/questionnaire/${datas.user.userId}`,
-        method: "get",
-        withCredentials: true,
-        headers: { "Content-Type": "application/json", token: datas.getToken() },
-    })
-        .then((response) => {
-            console.log("用户发布的所有问卷信息", response);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+const disabledDateFun = (time) => {
+    // console.log(time);
+    return time.getTime() > Date.now() - 8.64e6
+
 }
-getinfor();
 </script>
+
 <style scoped lang="less">
 .wapper {
     display: flex;
     flex-direction: column;
 
-    .title {
-        padding: 44px 0 0 46px;
-        font-size: 20px;
-        font-weight: 500;
-        letter-spacing: 0px;
-        line-height: 28px;
-        color: rgba(0, 0, 0, 1);
-        text-align: left;
-        vertical-align: top;
+    .top {
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+        margin: 44px 0 0 46px;
 
-        @media(max-width:@breakpoint) {
-            padding: 30px 0 0 5px;
+        .title {
+
+            font-size: 20px;
+            font-weight: 500;
+            letter-spacing: 0px;
+            line-height: 28px;
+            color: rgba(0, 0, 0, 1);
+            text-align: left;
+            vertical-align: top;
+
+            @media(max-width:@breakpoint) {
+                padding: 30px 0 0 5px;
+            }
+        }
+
+        >select {
+            position: absolute;
+            right: 255px;
+            height: 100%;
+            border-radius: 4px;
+            background: rgba(255, 255, 255, 1);
+            box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+            border: none;
+
+            &:focus-visible {
+                outline: none;
+            }
+        }
+
+        .time {
+            // position: absolute;
+            // right: 20px;
+            margin-right: 20px;
+
+            :deep(.el-date-editor) {
+                border-radius: 4px;
+                background: rgba(255, 255, 255, 1);
+                box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+                border: none;
+                --el-date-editor-width: 223px;
+
+                :deep(el-input__wrapper) {
+                    box-shadow: none;
+
+                    &:hover {
+                        box-shadow: none;
+                    }
+                }
+
+                // .el-input,
+                // .el-input__wrapper {
+                //     // width: 100px;
+                // }
+            }
         }
     }
 }
